de5b6c207bf06e99dada572e2115119fb9268147
nizar-aplikasi <nizar.aplikasi@gmail.com>
2025-07-26 13:06:57 +0700

    delete

--- models/login_request.go
+++
@@ -1,7 +0,0 @@
-// File: models/login_request.go
-package models
-
-// LoginRequest model untuk menerima data login
-type LoginRequest struct {
-	LoginForm string `json:"login_form" binding:"required"`
-}
